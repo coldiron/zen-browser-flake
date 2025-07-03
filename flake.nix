a515d2ac6ce1a93b82a28b8a17ba7b225efd7e87
Richard Mitsuk Lavitt <r.mitsuklavitt@gmail.com>
2025-07-03 08:52:59 -0500

    fix urls

--- flake.nix
+++ flake.nix
@@ -12,11 +12,11 @@
       version = "1.14.1b";
       downloadUrl = {
         "specific" = {
-          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
+          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
           sha256 = "sha256:bf9a3440884ed88a942016ea448e1cf58cf23f2a803d6de0e6b99d32840e1ecf";
         };
         "generic" = {
-          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
+          url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
           sha256 = "sha256:bf9a3440884ed88a942016ea448e1cf58cf23f2a803d6de0e6b99d32840e1ecf";
         };
       };
